28326f024062eac31bce7459161fafe630d1359a
catalina <catalinaruedaf@hotmail.com>
2026-06-30 17:01:30 +1000

    images appears on the home page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/shared/logo.svg";
+import logo from "/public/assets/shared/logo.svg";
+
 
 export default function Navigation(){
     return (
