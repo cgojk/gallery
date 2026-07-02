cd26b4757a4f969dd72f112b3e7a0ba216e72c39
catalina <catalinaruedaf@hotmail.com>
2026-07-02 11:54:31 +1000

    styling the principal page navigaiton an images

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,22 +6,15 @@ import logo from "/public/assets/shared/logo.svg";
 export default function Navigation(){
     return (
  <section className="navigation">
-   
-       <nav className="nav container">
-        <img src={logo} className="logo-image" loading="lazy" alt="gallery title logo " />
-        <Link to="/description_image" className="links__description">
-        <button  className="links__startslideshow">
-           START SLIDESHOW
-            </button>
-        </Link>
-        
-       </nav>
-    
-  
-     
-    
-       
-</section>
+    <nav className="nav container">
+          <img src={logo} className="logo-image" loading="lazy" alt="gallery title logo " />
+            <Link to="/description_image " className="links__description">
+          <button  className="links__startslideshow text-5">
+            START SLIDESHOW
+          </button>
+          </Link>
+      </nav>
+  </section>
 
   );
 }
